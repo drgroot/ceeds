24fdaad54a4b8e1dc5347d4433858a6534d6074f
Yusuf Ali <ali@yusuf.email>
2018-03-07 20:20:46 -0500

    Working deployment strategy with github pages.

--- deploy_ceeds.sh
+++ deploy_ceeds.sh
@@ -6,13 +6,22 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 git pull origin master
 cd themes/detox && git pull origin master
-rm -rf $DIR/public
+rm -rf $DIR/public/*
 rm -rf $DIR/content/post/
 
 # copy posts over
 /usr/bin/rclone copy google:documents/ceeds/ $DIR/content/post/
 
-# generate the website to static HTML
-# and deploy website 
+# build content
 cd $DIR && hugo --theme=detox
-/usr/bin/firebase deploy
+
+# deploy
+cd $DIR/public
+cp $DIR/CNAME .
+git init
+git remote add origin git@github.com:drgroot/ceeds.git
+git checkout -b gh-pages
+git add --all
+git config commit.gpgsign false
+git commit -m "Publishing to gh-pages"
+git push -f origin gh-pages
